11573774778ada2af04e6de0bd00f1c5ea3ea5e6
Konstantin <derhanov@gmail.com>
2018-11-12 12:06:11 +0200

    before use HttpKernel Component

--- src/app.php
+++ src/app.php
@@ -1,6 +1,7 @@
 <?php
 use Symfony\Component\Routing;
-use Symfony\Component\HttpFoundation\Response;
+use App\LeapYearController;
+require_once __DIR__ . '/../vendor/autoload.php';
 
 function is_leap_year($year = null) {
 	if (null === $year) {
@@ -13,13 +14,7 @@ function is_leap_year($year = null) {
 $routes = new Routing\RouteCollection();
 $routes->add('leap_year', new Routing\Route('/is_leap_year/{year}', array(
 	'year' => null,
-	'_controller' => function ($request) {
-		if (is_leap_year($request->attributes->get('year'))) {
-			return new Response('Yep, this is a leap year!');
-		}
-
-		return new Response('Nope, this is not a leap year.');
-	}
+	'_controller' => array(new LeapYearController(), 'index'),
 )));
 
 return $routes;
