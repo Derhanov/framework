3081296824d08d6c766342f51abd39878fd77a05
Konstantin <derhanov@gmail.com>
2018-11-13 11:05:03 +0200

    Unit Testing

--- src/Simplex/Framework.php
+++ src/Simplex/Framework.php
@@ -4,21 +4,32 @@ namespace Simplex;
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
-use Symfony\Component\HttpKernel\Controller\ControllerResolver;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\Matcher\UrlMatcherInterface;
+use Symfony\Component\HttpKernel\Controller\ControllerResolverInterface;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolverInterface;
 
 class Framework {
+//	protected $matcher;
+//	protected $controllerResolver;
+//	protected $argumentResolver;
 	protected $matcher;
-	protected $controllerResolver;
+	protected $resolver;
 	protected $argumentResolver;
 
-	public function __construct( UrlMatcher $matcher, ControllerResolver $controllerResolver, ArgumentResolver $argumentResolver ) {
-		$this->matcher            = $matcher;
-		$this->controllerResolver = $controllerResolver;
-		$this->argumentResolver   = $argumentResolver;
+//	public function __construct( UrlMatcher $matcher, ControllerResolver $controllerResolver, ArgumentResolver $argumentResolver ) {
+//		$this->matcher            = $matcher;
+//		$this->controllerResolver = $controllerResolver;
+//		$this->argumentResolver   = $argumentResolver;
+//	}
+	public function __construct(UrlMatcherInterface $matcher, ControllerResolverInterface $resolver, ArgumentResolverInterface $argumentResolver)
+	{
+		$this->matcher = $matcher;
+		$this->resolver = $resolver;
+		$this->argumentResolver = $argumentResolver;
 	}
+
+
 
 	public function handle( Request $request ) {
 		$this->matcher->getContext()->fromRequest( $request );
@@ -26,7 +37,7 @@ class Framework {
 		try {
 			$request->attributes->add( $this->matcher->match( $request->getPathInfo() ) );
 
-			$controller = $this->controllerResolver->getController( $request );
+			$controller = $this->resolver->getController( $request );
 			$arguments  = $this->argumentResolver->getArguments( $request, $controller );
 
 			return call_user_func_array( $controller, $arguments );
